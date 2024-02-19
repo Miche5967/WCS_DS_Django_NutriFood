2fba9a67869601182177f7bda8b2f30cc042efbd
Julien Michaut <julien.michaut.59@gmail.com>
2024-02-19 10:24:54 +0100

    Mise à jour settings.py pour configuration DATABASES : bdd  postgresql

--- src/NutriFood/settings.py
+++ src/NutriFood/settings.py
@@ -75,9 +75,17 @@ WSGI_APPLICATION = "NutriFood.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "NutriFood_BDD",
+        "USER": "Julien",
+        "PASSWORD": "Miche_postgre",
+        "HOST": "127.0.0.1",
+        "PORT": "5432",
     }
+    # "default": {
+    #     "ENGINE": "django.db.backends.sqlite3",
+    #     "NAME": BASE_DIR / "db.sqlite3",
+    # }
 }
 
 
